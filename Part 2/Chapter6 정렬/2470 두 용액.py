7d9a19c49e31a089770fe416d655a83275e49908
김민규 <minq0302@naver.com>
2022-07-22 13:24:23 +0900

    Solved 2470

--- Part 2/Chapter6 정렬/2470 두 용액.py
+++ Part 2/Chapter6 정렬/2470 두 용액.py
@@ -4,16 +4,28 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int,input().split()))
+arr.sort()
 
-tmp = sys.maxsize - 1
+tmp = sys.maxsize - 1 # 더한 값을 저장할 변수
+left = 0 # 왼쪽 포인터
+right = n-1 # 오른쪽 포인터
+one = 0 # 더할 왼쪽 수
+two = 0 # 더할 오른쪽 수
 
+while left < right: # 왼쪽 포인터가 오른쪽 포인터보다 작은 동안
+    temp = arr[left] + arr[right] # 왼쪽 포인터 값과 오른쪽 포인터 값을 더한다
+    res = abs(temp) # 더한 값을 절대값으로 저장
 
-for i in range(n):
-    for j in range(i+1,n): 
-        if abs(arr[i] + arr[j]) < abs(tmp):
-            tmp = arr[i] + arr[j]
-            res_arr = [arr[i],arr[j]]
+    if tmp > res: # 새로운 값이 기존의 값보다 작으면
+        tmp = res # tmp에 새로운 값을 저장
+        one = arr[left]
+        two = arr[right]
+    
+    # 두 용액의 합이 양수라면 오른쪽 포인터를 줄여 값을 작게한다.
+    if temp > 0:
+        right -= 1
+    # 두 용액의 합이 음수라면 왼쪽 포인터를 증가시켜 값을 크게한다.
+    else:
+        left += 1
 
-res_arr.sort()
-for i in res_arr:
-    print(i, end = ' ')
+print(one,two)
