ea5973809735a9347870f83facd2e6912b83f5f5
김민규 <minq0302@naver.com>
2022-07-24 22:45:40 +0900

    피보나치 메모이제이션

--- Part 2/Chapter7 이진탐색/7795 먹을 것인가 먹힐 것인가.py
+++ Part 2/Chapter7 이진탐색/7795 먹을 것인가 먹힐 것인가.py
@@ -1,4 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+def binary(array,target,start,end):
+    total = 0
+    mid = (start+end) // 2
+
+    
+
+t = int(input())
+n,m = map(int,input().split()) # a의 수 n, b의 수 m
+
+for i in range(t):
+    a_arr = list(map(int,input().split()))
+    b_arr = list(map(int,input().split()))
+    
+    a_arr.sort()
+    b_arr.sort()
