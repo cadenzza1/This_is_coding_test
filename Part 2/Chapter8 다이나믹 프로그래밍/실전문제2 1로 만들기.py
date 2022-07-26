0d55390e7efa2da27e3396c57807571689b664ef
김민규 <minq0302@naver.com>
2022-07-26 21:34:20 +0900

    Chapter8 실전문제2 1로 만들기

--- Part 2/Chapter8 다이나믹 프로그래밍/실전문제2 1로 만들기.py
+++ Part 2/Chapter8 다이나믹 프로그래밍/실전문제2 1로 만들기.py
@@ -3,24 +3,19 @@ input = sys.stdin.readline
 
 x = int(input())
 
-cnt = 0
+d = [0] * 30001 # 계산된 결과를 저장하기 위한 DP테이블 초기화
 
-while x != 1:
-    if x // 5 == 0:
-        x = x // 5
-        cnt += 1
-        continue
-    elif x // 3 == 0:
-        x = x // 3
-        cnt += 1
-        continue
-    elif x // 2 == 0:
-        x = x // 2
-        cnt += 1
-        continue
-    else:
-        x -= 1
-        cnt += 1
-        continue
+# 다이나믹 프로그래밍 진행 (Bottom-up)
+for i in range(2, x+1):
+     # 현재의 수에서 1을 빼는 경우
+    d[i] = d[i-1] + 1
+     # 현재의 수가 2로 나누어 떨어지는 경우
+    if i % 2 == 0:
+        d[i] = min(d[i] , d[i//2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i] , d[i//3] + 1)
+    if i % 5 == 0:
+        d[i] = min(d[i] , d[i//5] + 1)
 
-print(cnt)
+print(d[x])
+    
